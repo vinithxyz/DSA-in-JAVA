9c4032004ca7596ffbf113a9f8c552e33601d305
Vinith S <vinith00117@gmail.com>
2026-07-21 20:17:36 +0530

    Time: 1 ms (97.49%) | Memory: 47.3 MB (37.11%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -6,10 +6,7 @@ class Solution {
 
         for(int i=0;i<n;i++){
             ans[i] = nums[i];
-            
             ans[i + n] = nums[i];
-        
-
         }
         return ans;
         
