73ef3e23603683f5bbfe149fb28bec1746435050
Vinith S <vinith00117@gmail.com>
2026-07-30 23:39:11 +0530

    Time: 7 ms (31.94%) | Memory: 46.6 MB (16.51%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char s1[] = new char[s.length()];
+        char t1[] = new char[t.length()];
+
+        if (s.length() != t.length()) {
+                return false;
+            }
+
+        for(int i=0;i<s.length();i++){
+            s1[i] = s.charAt(i);
+            t1[i] = t.charAt(i);
+        }
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        if(Arrays.equals(s1, t1)){
+            return true;
+        }
+
+    return false;
+        
+    }
+}
