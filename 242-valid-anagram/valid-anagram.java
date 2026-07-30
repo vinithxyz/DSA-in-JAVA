61f9ad8fb6e0762bc81a4ab693e0aac92b0161c5
Vinith S <vinith00117@gmail.com>
2026-07-30 23:47:54 +0530

    Time: 7 ms (31.94%) | Memory: 46.5 MB (25.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -16,8 +16,6 @@ class Solution {
         Arrays.sort(s1);
         Arrays.sort(t1);
 
-        
-        return Arrays.equals(s1, t1);
-        
+     return Arrays.equals(s1, t1);        
     }
 }
