20b7ca6a6c4bb8c70e0cfed89e392b6bd3538bb5
Vinith S <vinith00117@gmail.com>
2026-08-04 19:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (74.42%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         
-        return (n&(n-1))==0 && n>0;    // logic is n=4
-                                // 4 = 100, 3=011 if we do and of 4&3 we will get 0 
-       }                         // n = 8  8=1000 7=0111 8&7=0 so n&(n-1)=0 for pow of 2
-
-    }
+         return (n&(n-1))==0 && n>0;    // logic is n=4
+                                        // 4 = 100, 3=011 if we do and of 4&3 we will get 0 
+       }                                // n = 8  8=1000 7=0111 8&7=0 so n&(n-1)=0 for pow of 2
+ }
