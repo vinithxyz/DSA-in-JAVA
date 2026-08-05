e7a9a958aef18cfddea2ce041994f1df59500688
Vinith S <vinith00117@gmail.com>
2026-08-05 19:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.25%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans;
-        Arrays.sort(nums);
-
+        int ans = nums.length;
+        
         for(int i=0;i<nums.length;i++){
-            ans =i^nums[i];
-            if(ans != 0){
-                return i;
-            }
-           
+            ans^=(i^nums[i]);
         }
-    return nums.length;
+        return ans;
     }
 }
