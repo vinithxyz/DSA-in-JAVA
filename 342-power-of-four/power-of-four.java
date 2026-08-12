f3377175d4546735df9a802060ccf1eb61ec62de
Vinith S <vinith00117@gmail.com>
2026-08-12 20:03:27 +0530

    Time: 1 ms (89.78%) | Memory: 42.4 MB (70.81%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1 || n==4){
-            return true;
-        }
-        else if(n==0 || n==144 || n==400 || n==576){
-            return false;
-        }
-        else if( Math.sqrt(n)%4==0 && n%4==0 && n>4){
-            return true;
-        }
-        return false;
+        if (n<=0) return false;
+        if (n==1) return true;
+        if ((n%4) != 0) return false;
+        return isPowerOfFour(n/4);
+        
     }
 }
