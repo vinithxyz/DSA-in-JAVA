301251f8b90a213f0cf8a5a3e50c79e4be013ade
Vinith S <vinith00117@gmail.com>
2026-08-23 21:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (56.24%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -3,12 +3,12 @@ class Solution {
         int max = Integer.MIN_VALUE;
        
         for(int i=0;i<accounts.length;i++){
-            int currSum = 0;
+            int currSum = 0;                       //as soon as i increments currSum will become 0
             for(int j=0;j<accounts[0].length;j++){
                 currSum+=accounts[i][j];
-              //  max = Math.max(max,currSum);
+              //  max = Math.max(max,currSum);    we can also use this but runtime will be 1ms
             }
-             if(currSum>=max){
+             if(currSum>=max){                   //this is inside a for loop  and it will find the lasgest sum
                  max = currSum;
                 }
         }
