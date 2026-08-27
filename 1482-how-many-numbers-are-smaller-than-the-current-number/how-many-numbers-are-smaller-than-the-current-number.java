294faf5961c82ae7c353c00188e98e147e60617a
Vinith S <vinith.s.engineer@gmail.com>
2026-08-27 19:49:56 +0530

    Time: 8 ms (63.08%) | Memory: 45.6 MB (50.77%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int count = 0;
             for(int j=0;j<nums.length;j++){
-                if(nums[j]<nums[i] && j!=i){
+                if(nums[j]<nums[i]){
                     count++;
                 }
             }
