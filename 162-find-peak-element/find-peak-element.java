554a3c16cbb2487f1db932464793effccaacc54a
Vinith S <vinith.s.engineer@gmail.com>
2026-09-04 18:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (19.77%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -16,3 +16,91 @@ class Solution {
         return l;
     }
 }
+
+/*
+Dry Run:
+nums = [1, 2, 1, 3, 5, 6, 4]
+        0  1  2  3  4  5  6
+
+Initial:
+l = 0
+r = 6
+
+--------------------------------------------------
+
+Iteration 1:
+mid = l + (r-l)/2
+    = 0 + (6-0)/2
+    = 3
+
+nums[mid]     = nums[3] = 3
+nums[mid + 1] = nums[4] = 5
+
+3 > 5 → false
+
+So:
+l = mid + 1
+l = 4
+
+Now:
+l = 4
+r = 6
+
+--------------------------------------------------
+
+Iteration 2:
+mid = 4 + (6-4)/2
+    = 5
+
+nums[mid]     = nums[5] = 6
+nums[mid + 1] = nums[6] = 4
+
+6 > 4 → true
+
+So:
+r = mid
+r = 5
+
+Now:
+l = 4
+r = 5
+
+--------------------------------------------------
+
+Iteration 3:
+mid = 4 + (5-4)/2
+    = 4
+
+nums[mid]     = nums[4] = 5
+nums[mid + 1] = nums[5] = 6
+
+5 > 6 → false
+
+So:
+l = mid + 1
+l = 5
+
+Now:
+l = 5
+r = 5
+
+--------------------------------------------------
+
+Loop condition:
+while(l < r)
+
+5 < 5 → false
+
+Loop ends.
+
+return l;
+
+return 5
+
+nums[5] = 6
+
+6 is a peak because:
+5 < 6 > 4
+
+Answer = 5
+*/
